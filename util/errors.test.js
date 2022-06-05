49ffa35bb250ad010de3152ec7d8fd05cc970f10
Sean <evans.seana@gmail.com>
2022-06-05 18:05:34 +0100

    Section 8, complete

--- util/errors.test.js
+++ util/errors.test.js
@@ -1,5 +1,5 @@
-import { describe, it } from "vitest";
-import { HttpError } from "./errors";
+import { describe, it, expect } from "vitest";
+import { HttpError, ValidationError } from "./errors";
 
 describe("class HttpError", () => {
   it("should contain the provided status code, message and data", () => {
@@ -7,6 +7,31 @@ describe("class HttpError", () => {
     const testMessage = "test";
     const testData = { key: "test" };
 
-    const testError = new HttpError();
+    const testError = new HttpError(testStatus, testMessage, testData);
+
+    expect(testError.statusCode).toBe(testStatus);
+    expect(testError.message).toBe(testMessage);
+    expect(testError.data).toBe(testData);
+  });
+
+  it("should contain undefined as data if no data is provided", () => {
+    const testStatus = 1;
+    const testMessage = "test";
+
+    const testError = new HttpError(testStatus, testMessage);
+
+    expect(testError.statusCode).toBe(testStatus);
+    expect(testError.message).toBe(testMessage);
+    expect(testError.data).not.toBeDefined();
+  });
+});
+
+describe("class ValidationError", () => {
+  it("should contain the provided message", () => {
+    const testMessage = "test";
+
+    const testError = new ValidationError(testMessage);
+
+    expect(testError.message).toBe(testMessage);
   });
 });
